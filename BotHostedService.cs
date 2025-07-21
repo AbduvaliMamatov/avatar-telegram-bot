80bd574f51883317c41d3f731a5179209e519dd5
Abduvali Mamatov <thebestlittleprince@gmail.com>
2025-07-21 16:05:29 +0500

    Fix: run bot in background to prevent Railway crash

--- BotHostedService.cs
+++ BotHostedService.cs
@@ -5,6 +5,9 @@ using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace TelegramBot;
 
 public class BotHostedService(
@@ -13,24 +16,34 @@ public class BotHostedService(
     IUpdateHandler updateHandler
     ) : IHostedService
 {
-    public async Task StartAsync(CancellationToken cancellationToken)
-    {
-        var me = await botClient.GetMe(cancellationToken);
-        logger.LogInformation("🎉 {bot} has started successfully.", $"{me.FirstName} - {me.Username}");
+    private CancellationTokenSource? _cts;
 
-        await botClient.ReceiveAsync(
-            updateHandler,
-            new ReceiverOptions
-            {
-                DropPendingUpdates = true,
-                AllowedUpdates = new[] { UpdateType.Message, UpdateType.CallbackQuery }
-            },
-            cancellationToken: cancellationToken);
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+        Task.Run(async () =>
+        {
+            var me = await botClient.GetMe(_cts.Token);
+            logger.LogInformation("🎉 {bot} has started successfully.", $"{me.FirstName} - {me.Username}");
+
+            await botClient.ReceiveAsync(
+                updateHandler,
+                new ReceiverOptions
+                {
+                    DropPendingUpdates = true,
+                    AllowedUpdates = new[] { UpdateType.Message, UpdateType.CallbackQuery }
+                },
+                cancellationToken: _cts.Token);
+        });
+
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
         logger.LogInformation("{service} is exiting...", nameof(BotHostedService));
+        _cts?.Cancel();
         return Task.CompletedTask;
     }
 }
